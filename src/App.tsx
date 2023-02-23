73cca28bcec88dc9d2c4f4eaa16ae6b4c00bf45e
multida <wow_da65@naver.com>
2023-02-23 22:51:54 +0900

    🏷️ Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,17 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-color: ${(proprs) => proprs.theme.bgColor};
+`;
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 function App() {
-  //1. input의 value를 state와 연결하기를 원한다.
-  const [value, setValue] = useState("");
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    console.log(event.currentTarget.value);
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("value", value);
-  };
-
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
