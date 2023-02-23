a2467d56978c3a3c72dc4f9949978932bc96f191
multida <wow_da65@naver.com>
2023-02-23 22:09:26 +0900

    🏷️

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
-import styled from "styled-components";
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  //1. input의 value를 state와 연결하기를 원한다.
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    console.log(event.currentTarget.value);
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("value", value);
+  };
+
   return (
     <div>
-      <Circle bgColor="orange" />
-      <Circle text="Im here" borderColor="black" bgColor="red" />
+      <form onSubmit={onSubmit}>
+        <input
+          value={value}
+          onChange={onChange}
+          type="text"
+          placeholder="username"
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
